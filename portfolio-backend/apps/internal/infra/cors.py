85ba961d237e985cd66ad209c6235e582f9c43b2
GitEngHar <harukii1999@gmail.com>
2025-08-13 00:17:03 +0900

    good fix

--- portfolio-backend/apps/internal/infra/cors.py
+++ portfolio-backend/apps/internal/infra/cors.py
@@ -1,12 +1,13 @@
 from fastapi import FastAPI
 from starlette.middleware.cors import CORSMiddleware
 
+ALLOWED_ORIGIN = "https://1-f-honi.github.io"
+
 def cors_setting(app: FastAPI):
     app.add_middleware(
         CORSMiddleware,
-        allow_origins=["*"],
-        allow_credentials=True,
-        allow_methods=["*"],
-        allow_headers=["*"],
+        allow_origins=[ALLOWED_ORIGIN],
+        allow_methods=["POST", "OPTIONS"],  # 使うメソッドを列挙
+        allow_headers=["Content-Type", "Authorization"],
     )
     return
